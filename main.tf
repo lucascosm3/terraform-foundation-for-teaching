fd97afcd5b2780780e7e5070f497064ad06fd39f
Lucas Cosme <lcds1944@gmail.com>
2026-03-03 06:59:32 -0300

    feat(main): add provider aws config + required version terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 6.34.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
