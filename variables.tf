e588b3af8601cd4187f18fc5cbf4af8cc6430f66
Lucas Cosme <lcds1944@gmail.com>
2026-03-04 22:50:02 -0300

    feat(variables.tf): add description in variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "environment" {
   type = string
   default = "dev"
+  description = "Environment of the resources"
 }
 
 variable "owner" {
   type = string
   default = "lucascosm3"
+  description = "Owner of the resources"
 }
