1519320968c4a0c95ff1e837635dd2e39399d4d5
Lucas Cosme <lcds1944@gmail.com>
2026-03-06 08:21:43 -0300

    chore: utilizing .tfvars

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "environment" {
   type        = string
-  default     = "dev"
   description = "Environment of the resources"
 }
 
 variable "account_id" {
   type        = string
   description = "AWS Account ID"
-  default     = "091708608334"
 }
