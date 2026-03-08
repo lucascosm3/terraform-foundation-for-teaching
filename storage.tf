795eb54ac85463f5b555e659bedc3b4bc67c0cbf
Lucas Cosme <lcds1944@gmail.com>
2026-03-07 22:00:22 -0300

    feat(storage.tf): create s3 for remote-state + versioning configuration

--- storage.tf
+++ storage.tf
@@ -1,9 +1,16 @@
-resource "aws_s3_bucket" "teacher_bucket" {
-  bucket = "terraform-foundation-for-teaching-${var.environment}"
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "terraform-state-for-teaching-${var.environment}"
 
   tags = {
-    Name        = "terraform-foundation-for-teaching-${var.environment}"
+    Name        = "terraform-state-${var.environment}"
     Environment = var.environment
     AccountID   = var.account_id
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
