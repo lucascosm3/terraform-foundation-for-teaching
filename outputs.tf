f5dc334d3b6eeffb3fe0011b29b3415daf0db7fe
Lucas Cosme <lcds1944@gmail.com>
2026-03-07 22:01:43 -0300

    chore(outputs.tf): change ref bucket outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "teacher_bucket_id" {
-  value = aws_s3_bucket.teacher_bucket.id
+output "terraform_state_bucket_id" {
+  value = aws_s3_bucket.terraform_state_bucket.id
 }
 
-output "teacher_bucket_arn" {
-  value = aws_s3_bucket.teacher_bucket.arn
+output "terraform_state_bucket_arn" {
+  value = aws_s3_bucket.terraform_state_bucket.arn
 }
 
-output "teacher_bucket_name" {
-  value = aws_s3_bucket.teacher_bucket.bucket
+output "terraform_state_bucket_name" {
+  value = aws_s3_bucket.terraform_state_bucket.bucket
 }
