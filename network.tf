3fddf2236bab23b86980dad0647ab0ec815f3e4b
Lucas Cosme <lcds1944@gmail.com>
2026-03-16 11:19:24 -0300

    lint(network): terraform fmt

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 module "vpc_network_np" {
   source = "./modules/vpc"
 
-  vpc_name = "vpc_network_np"
-  vpc_cidr = "10.0.0.0/16"
+  vpc_name           = "vpc_network_np"
+  vpc_cidr           = "10.0.0.0/16"
   enable_nat_gateway = false
   public_subnets = {
     public_np = {
@@ -15,8 +15,8 @@ module "vpc_network_np" {
 module "vpc_network_prod" {
   source = "./modules/vpc"
 
-  vpc_name = "vpc_network_prod"
-  vpc_cidr = "10.1.0.0/16"
+  vpc_name           = "vpc_network_prod"
+  vpc_cidr           = "10.1.0.0/16"
   enable_nat_gateway = false
   public_subnets = {
     public_prod = {
