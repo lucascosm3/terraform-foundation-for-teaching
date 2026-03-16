8232de1fd29644b455ebb0a2c24fd5a74a4484e2
Lucas Cosme <lcds1944@gmail.com>
2026-03-16 11:15:20 -0300

    chore(modules/vpc/main): remove unused block private

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -37,24 +37,3 @@ resource "aws_subnet" "private" {
     Name = "${var.vpc_name}-private-${each.key}"
   }
 }
-
-# Private Route Table
-resource "aws_route_table" "private" {
-  count  = var.enable_nat_gateway ? 1 : 0
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this[0].id 
-  }
-
-  tags = { Name = "${var.vpc_name}-private-rt" }
-}
-
-# Private Route Table Association
-resource "aws_route_table_association" "private" {
-  for_each = var.enable_nat_gateway ? var.private_subnets : {}
-
-  subnet_id      = aws_subnet.private[each.key].id
-  route_table_id = aws_route_table.private[0].id
-}
