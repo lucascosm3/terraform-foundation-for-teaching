938d957ad73ec3c37ecf06cc55bb2294b1c77ab7
Lucas Cosme <lcds1944@gmail.com>
2026-03-18 11:48:58 -0300

    chore(storage): remove bucket unused

--- storage.tf
+++ storage.tf
@@ -22,16 +22,3 @@ module "s3_bucket_state" {
     enabled = true
   }
 }
-
-module "s3_bucket_state_2" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "terraform-20260318140347320800000001"
-
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerEnforced"
-
-  versioning = {
-    enabled = true
-  }
-}
