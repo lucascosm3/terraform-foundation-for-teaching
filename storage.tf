794a51a2f3094a2d9b5305abb1118f03a8e66509
Lucas Cosme <lcds1944@gmail.com>
2026-03-18 10:59:48 -0300

    chore: removing a bucket that does not use a remote module.

--- storage.tf
+++ storage.tf
@@ -1,29 +1,10 @@
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "terraform-state-for-teaching-${var.environment}"
-
-  tags = {
-    Name        = "terraform-state-${var.environment}"
-    Environment = var.environment
-    AccountID   = var.account-id
-    Managed-by  = var.managed-by
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-module "s3_bucket_state" {
+module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "terraform-state-for-teaching-${var.environment}"
-  acl    = "private"
 
   control_object_ownership = true
-  object_ownership         = "ObjectWriter"
+  object_ownership         = "BucketOwnerEnforced"
 
   versioning = {
     enabled = true
