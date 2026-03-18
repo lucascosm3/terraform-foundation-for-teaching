a920970f6cd38cc31298af67998c77037e0e1e5e
Lucas Cosme <lcds1944@gmail.com>
2026-03-18 11:44:34 -0300

    feat(storage): add block moved to bucket s3 + add block import to s3 not present in state

--- storage.tf
+++ storage.tf
@@ -1,7 +1,32 @@
-module "s3_bucket" {
+import {
+  to = module.s3_bucket_state_2.aws_s3_bucket.this[0]
+  id = "terraform-20260318140347320800000001"
+}
+
+moved {
+    from = module.s3_bucket
+    to   = module.s3_bucket_state
+}
+
+
+
+module "s3_bucket_state" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "terraform-state-for-teaching-${var.environment}"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "s3_bucket_state_2" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "terraform-20260318140347320800000001"
 
   control_object_ownership = true
   object_ownership         = "BucketOwnerEnforced"
