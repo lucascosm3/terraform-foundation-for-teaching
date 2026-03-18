7d9ce3563b926fcd1d7857f7ee2b3d2635b2a6e2
Lucas Cosme <lcds1944@gmail.com>
2026-03-18 09:52:51 -0300

    feat(network): create new vpc np with remote-module aws vpc

--- network.tf
+++ network.tf
@@ -25,3 +25,22 @@ module "vpc_network_prod" {
     }
   }
 }
+
+module "vpc_np_module_remote_aws" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-np-module-remote-aws-${var.environment}"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    managed-by = "${var.managed-by}"
+    environment = "${var.environment}"
+  }
+}
