86b87efd94f16292d2d7ffaa3053121bc40d562c
Lucas Cosme <lcds1944@gmail.com>
2026-03-19 13:27:27 -0300

    feat: add resources test depends_on

--- storage.tf
+++ storage.tf
@@ -11,25 +11,25 @@ module "s3_bucket_state" {
   }
 }
 
-# # Bucket to store the script instance to test of EC2 user data with depends_on
-# module "s3_bucket_script_instance" {
-#   source = "terraform-aws-modules/s3-bucket/aws"
+# Bucket to store the script instance to test of EC2 user data with depends_on
+module "s3_bucket_script_instance" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-#   bucket = "terraform-script-instance-for-teaching-${var.environment}"
+  bucket = "terraform-script-instance-for-teaching-${var.environment}"
 
-#   control_object_ownership = true
-#   object_ownership         = "BucketOwnerEnforced"
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
 
-#   versioning = {
-#     enabled = true
-#   }
-# }
+  versioning = {
+    enabled = true
+  }
+}
 
-# # Object to store the script instance to test of EC2 user data with depends_on
-# resource "aws_s3_object" "setup-ec2-sh" {
-#   bucket     = "terraform-script-instance-for-teaching-${var.environment}"
-#   key        = "setup-ec2.sh"
-#   source     = "/home/lucas/Workplaces/Labs/scripts/setup-ec2.sh"
-#   etag       = filemd5("/home/lucas/Workplaces/Labs/scripts/setup-ec2.sh")
-#   depends_on = [module.s3_bucket_script_instance]
-# }
+# Object to store the script instance to test of EC2 user data with depends_on
+resource "aws_s3_object" "setup-ec2-sh" {
+  bucket     = "terraform-script-instance-for-teaching-${var.environment}"
+  key        = "setup-ec2.sh"
+  source     = "/home/lucas/Workplaces/Labs/scripts/setup-ec2.sh"
+  etag       = filemd5("/home/lucas/Workplaces/Labs/scripts/setup-ec2.sh")
+  depends_on = [module.s3_bucket_script_instance]
+}
