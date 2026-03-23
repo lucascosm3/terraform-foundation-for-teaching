6a06e2559613694031fb3ea2d029770a11aeb569
Lucas Cosme <lcds1944@gmail.com>
2026-03-23 08:20:38 -0300

    cleanup(network): remove unused `vpc_network_prod` module block

--- network.tf
+++ network.tf
@@ -12,20 +12,6 @@ module "vpc_network_np" {
   }
 }
 
-module "vpc_network_prod" {
-  source = "./modules/vpc"
-
-  vpc_name           = "vpc_network_prod"
-  vpc_cidr           = "10.1.0.0/16"
-  enable_nat_gateway = false
-  public_subnets = {
-    public_prod = {
-      cidr = "10.1.1.0/24"
-      az   = "us-east-1a"
-    }
-  }
-}
-
 module "vpc_np_module_remote_aws" {
   source = "terraform-aws-modules/vpc/aws"
 
