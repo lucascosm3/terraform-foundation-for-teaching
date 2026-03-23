60a0376c7c24b758a03a1aab76a4c91b532dd002
Lucas Cosme <lcds1944@gmail.com>
2026-03-23 08:21:34 -0300

    cleanup: remove EC2 instance provisioning, IAM roles, security group, setup script, and related outputs.

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "instance_public_ip" {
-#   description = "Public IP of the EC2 instance"
-#   value       = aws_instance.web.public_ip
-# }
-
-# output "instance_web_url" {
-#   description = "Clickable URL to access the web server"
-#   value       = "http://${aws_instance.web.public_ip}"
-# }
--- scripts/setup-ec2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Atualiza os pacotes e instala o Apache no Ubuntu
-sudo apt-get update -y
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-
-# Cria uma página customizada
-echo "<h1>Deploy via Terraform com depends_on realizado com sucesso!</h1>" | sudo tee /var/www/html/index.html
