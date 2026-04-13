cc68d3de9bd073678391b73041aec8d45f7deeee
Lucas Cosme <lcds1944@gmail.com>
2026-04-13 08:15:55 -0300

    refactor: remove local vpc module and clean up network configuration

--- network.tf
+++ network.tf
@@ -1,18 +1,3 @@
-# VPC with module local
-module "vpc_network_np" {
-  source = "./modules/vpc"
-
-  vpc_name           = "vpc_network_np"
-  vpc_cidr           = "10.0.0.0/16"
-  enable_nat_gateway = false
-  public_subnets = {
-    public_np = {
-      cidr = "10.0.1.0/24"
-      az   = "us-east-1a"
-    }
-  }
-}
-
 # VPC with module remote AWS
 module "vpc_np_module_remote_aws" {
   source = "terraform-aws-modules/vpc/aws"
